e3352cce44ef7ddec654cfed302bff3d9a325d4b
Shuen Yasui <shuenyasui@gmail.com>
2019-05-28 21:58:22 -0700

    Branch to implement React to website. Initial boilerplate.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProjectEntry from './ProjectEntry';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          React Page
+        </h1>
+        <ProjectEntry />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ProjectEntry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProjectEntry extends Component {
+  render() {
+    return (
+      <div className="projectEntry">
+        <div>Project Entry</div>
+      </div>
+    );
+  }
+}
+
+export default ProjectEntry
