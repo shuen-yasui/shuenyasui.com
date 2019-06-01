eba1d778808452d6cd02492f71e2a7cbd70bd75c
Shuen Yasui <shuenyasui@gmail.com>
2019-06-01 00:44:04 -0700

    Added feature to display elements from JSON data. Add link element factory.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,42 @@ import React from 'react';
 import ProjectEntry from './ProjectEntry';
 
 function App() {
+  let state = {
+    data: [
+      {
+        id: 1,
+        title: "title1",
+        body: "body1",
+        link: null
+      },
+      {
+        id: 2,
+        title: "title2",
+        body: "body2",
+        link: "http://www.google.com"
+      }
+    ],
+    current: {}
+  }
+  let data = state.data.map((d) => {
+    let outputBody = [];
+    outputBody.push(d.body);
+    if (d.link) {
+      outputBody.push(React.createElement('a',{ key:d.link, href:d.link},d.link));
+    }
+      return (
+        <div key={d.id}>
+            <div>{d.title}</div>
+            <div>{outputBody}</div>
+        </div>
+      );
+  });
   return (
     <div className="App">
       <header className="App-header">
         <h1>
           React Page
+          { data }
         </h1>
         <ProjectEntry />
       </header>
